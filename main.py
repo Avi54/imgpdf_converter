1846949b8252b5e927c4649e527d826ec78fea11
Avi <avi.dhall54@gmail.com>
2022-05-06 22:39:08 +0530

    order correct if numbered

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ dir_path = input('directory path (images location): ')
 save_path = input('directory path for saving: ') 
 pdf_name = input('file name: ') + '.pdf'
 
-images_list = []
+image_paths = []
 for root, dirs, files in walk(path.abspath(dir_path)):
     for file in files:
         image_path = path.join(root, file)
-        img = Image.open(image_path)
-        images_list.append(img.convert('RGB'))
+        image_paths.append(image_path)
+
+image_paths.sort()
+
+images_list = []
+for file_path in image_paths:
+    img = Image.open(file_path)
+    images_list.append(img.convert('RGB'))
 
 first_image = images_list[0]
 del images_list[0]
