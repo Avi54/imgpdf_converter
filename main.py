40689c1546b6972c9234ce193e8c4fe6374a55bc
Avi Dhall <avi@Avis-MacBook-Pro.local>
2023-08-16 21:58:06 +0530

    Add functions: Remove hidden files that are not images

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from PIL import Image
-from os import walk, path
+from os import walk, path, listdir, remove
+
+def is_image(filename):
+    try:
+        Image.open(filename)
+        return True
+    except:
+        return False
+
+def remove_non_image_files(folder_path):
+    for filename in listdir(folder_path):
+        file_path = path.join(folder_path, filename)
+        if path.isfile(file_path) and not is_image(file_path):
+            remove(file_path)
+
 
 dir_path = input('directory path (images location): ')
 save_path = input('directory path for saving: ')
 pdf_name = input('file name: ') + '.pdf'
 
+remove_non_image_files(dir_path)
+
 image_paths = []
+
 for root, dirs, files in walk(path.abspath(dir_path)):
     for file in files:
         image_path = path.join(root, file)
